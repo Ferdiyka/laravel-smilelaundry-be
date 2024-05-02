af49259aa2f73946f44589ca82185a9787e2c7c6
Andyka Putra <andykaputra219@gmail.com>
2024-05-02 21:35:54 +0700

    server hilang

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,6 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('pages.auth.login');
